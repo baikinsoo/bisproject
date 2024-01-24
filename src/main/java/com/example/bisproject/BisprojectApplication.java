470307f144c4360f86d4927aee51cbcb23c6d6d3
baikinsoo <baikinsoo95@gmail.com>
2024-01-24 21:17:52 +0900

    Initial commit

---
+++ src/main/java/com/example/bisproject/BisprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BisprojectApplication.class, args);
+    }
+
+}
